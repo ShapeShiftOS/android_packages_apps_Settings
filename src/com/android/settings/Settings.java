fd4183839a7bb8347315ef0a35adf39e83bf23b5
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-03 14:18:10 -0300

    Settings: Changes for LiveDisplay
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_packages_apps_LineageParts
    
    Syberia: Forward port to 10 and migrate to androidx
    
    Squashed commits:
    
    commit f4795d67d018d99b1cf724360bee278d66bbebeb
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Tue Jan 14 02:39:24 2020 +0000
    
        Settings: Allow removing livedisplay pref screen with config_enableLiveDisplay
    
        Change-Id: I17103ad116d1d26855fb47fdee1fa6cdc914e053
    
    commit 98638aef74e0a6c1dab2a45a4582eb8daef0b2e9
    Author: Han Wang <416810799@qq.com>
    Date:   Fri May 1 15:41:46 2020 +0200
    
        Settings: Remove obselete PreferenceCategory for LiveDisplay
    
         * This avoids showing empty 'title' which looks ugly.
    
        Change-Id: I34c655c94a26fc5907620fc9b82b84852fe741be
    
    commit 36d8f89c63a38f182d69160264121c458a768b10
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Apr 11 14:34:40 2020 +0100
    
        LiveDisplaySettings: Hide Reading Mode if wellbeing app is enabled
    
        Google's wellbeing app provides Wind Down feature along with
        a grayscale option. Remove our preference to avoid having
        duplicate features.
    
        Change-Id: Ib20a0544a778ace385cb45ab2518a44420fb9bf6
    
    commit 6750d003c074d46df1200b29ba674e5748b29ee7
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Wed Aug 7 00:20:42 2019 +0800
    
        LiveDisplaySettings: Hide automatic outdoor mode preference on HWC2
    
         * Unconditionally enabled on HWC2.
    
        Change-Id: Iebc39ba7d9be2ad83425b70fdc0542b8e4be6104
    
    commit fb79eac7755c11f7b42ce6c432b505b2f4071869
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Tue Aug 6 23:33:11 2019 +0800
    
        LiveDisplaySettings: Reenable display mode preference for outdoor mode
    
        This reverts commit 7998c557e7f6a8749052f5bd7d7251dbc21d10ce.
    
        Change-Id: Iebcc6bcb8d29e8b61218a93d1856dd710e263f47
    
    commit 4491cdf82a0fafe1c0558d35044de8fd58ebd43f
    Author: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Date:   Sun Jun 2 18:32:11 2019 -0300
    
        Settings: Don't index display mode and color temperature on HWC2
    
        Change-Id: Iec23d14c6a7256432abc9a9bc313c303b88178c5
    
    commit e9511a67d15e02a2e47d5f4bd71529e12251dcca
    Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Date:   Fri Jul 5 20:27:23 2019 -0300
    
        LiveDisplaySettings: Fix outdoor mode preference on hwc2
    
        Change-Id: I7ecbc68cb7bc2eb70544f8f5bc3e3d9d2943e4d7
    
    commit 10e1b8b237261033282e9e4ee02b9048fce03c53
    Author: Rashed Abdel-Tawab <rashed@linux.com>
    Date:   Sat Jun 1 19:21:50 2019 +0530
    
        livedisplay: Don't show display mode and color tempertature on HWC2
    
        Change-Id: Ib60e11bdb79a233c961d00c0968922264c042011
    
    commit 3ac1021fb707c5bee611529ed90184cae6513e9d
    Author: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Date:   Mon May 6 10:53:01 2019 -0300
    
        LiveDisplay: Add illustration into picture and color adjustment
    
        Change-Id: I923ae55f2b4022bfba4292a6825ee75309e50239
    
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Change-Id: Iba83c078ff2e7eb0ae56eac2758ce658da3eb493

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -247,4 +247,8 @@ public class Settings extends SettingsActivity {
 
     // Gaming mode
     public static class GamingModeActivity extends SettingsActivity {}
+
+    // LiveDisplay
+    public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
+
 }
