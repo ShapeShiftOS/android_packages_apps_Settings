6b114ab82944f15f6632355521310ba64edb68af
00day0 <therandomuser11@gmail.com>
2019-12-21 22:24:07 +0100

    ExUI version: show if build is official or not + change packagename for ota updates
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/ExUIVersionDetailPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/ExUIVersionDetailPreferenceController.java
@@ -42,6 +42,7 @@ public class ExUIVersionDetailPreferenceController extends BasePreferenceControl
     private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
     private static final String KEY_EXUI_VERSION_PROP = "org.extendedui.version";
+    private static final String KEY_EXUI_BUILDTYPE_PROP = "org.extendedui.build_type";
 
     private final UserManager mUserManager;
     private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
@@ -63,7 +64,7 @@ public class ExUIVersionDetailPreferenceController extends BasePreferenceControl
     @Override
     public CharSequence getSummary() {
         return SystemProperties.get(KEY_EXUI_VERSION_PROP,
-                mContext.getString(R.string.unknown));
+                mContext.getString(R.string.unknown)) + "-" + SystemProperties.get(KEY_EXUI_BUILDTYPE_PROP, mContext.getString(R.string.unknown));
     }
 
     @Override
