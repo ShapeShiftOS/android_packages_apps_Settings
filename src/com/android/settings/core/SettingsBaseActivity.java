dd5a262678dc4c5b9a27d11f745007e1d312c373
Lucchetto <lucchetto.tie@live.com>
2019-12-28 17:42:02 +0100

    SettingsBaseActivity: draw behind the toolbar
    Change-Id: Iea6da1455b563de79a4071ad562db4c62fed41d9
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- src/com/android/settings/core/SettingsBaseActivity.java
+++ src/com/android/settings/core/SettingsBaseActivity.java
@@ -35,6 +35,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.FrameLayout;
 import android.widget.Toolbar;
 
 import androidx.fragment.app.FragmentActivity;
@@ -84,12 +85,30 @@ public class SettingsBaseActivity extends FragmentActivity {
         }
         super.setContentView(R.layout.settings_base_layout);
 
+        final View decorView = getWindow().getDecorView();
+        final ViewGroup root = (ViewGroup) decorView.findViewById(android.R.id.content);
+
         final Toolbar toolbar = findViewById(R.id.action_bar);
         if (theme.getBoolean(android.R.styleable.Theme_windowNoTitle, false)) {
             toolbar.setVisibility(View.GONE);
             return;
         }
         setActionBar(toolbar);
+        
+        root.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        root.setOnApplyWindowInsetsListener((v, insets) -> {
+            final FrameLayout contentFrame = findViewById(R.id.content_frame);
+            final TypedArray styledAttributes = getTheme().obtainStyledAttributes(
+                new int[] { android.R.attr.actionBarSize });
+            final int actionBarSize = (int) styledAttributes.getDimension(0, 0);
+            styledAttributes.recycle();
+
+            toolbar.setPadding(0, insets.getSystemWindowInsetTop(), 0, 0);
+            contentFrame.setPadding(0, insets.getSystemWindowInsetTop() + actionBarSize, 0, 0);
+            return insets;
+        });
 
         /*baseSpacer = findViewById(R.id.settings_submenu_spacer);
         baseMainLayout = findViewById(R.id.base_content_scrollable_container);
