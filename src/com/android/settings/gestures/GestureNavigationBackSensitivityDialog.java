d566d637861390e557df111999e746d50cd5dcbb
maxwen <max.weninger@gmail.com>
2020-01-22 02:02:20 +0100

    [2/2] Settings: dont block IME space for back gesture
    Change-Id: Iec0d223ecaebdae7123c3188f7963a83cf9be27d

--- src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
+++ src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
@@ -44,8 +44,9 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
     private static final String KEY_BACK_SENSITIVITY = "back_sensitivity";
     private static final String KEY_BACK_HEIGHT = "back_height";
     private static final String KEY_HOME_HANDLE_SIZE = "home_handle_width";
+    private static final String KEY_BACK_BLOCK_IME = "back_block_ime";
 
-    public static void show(SystemNavigationGestureSettings parent, int sensitivity, int height, int length) {
+    public static void show(SystemNavigationGestureSettings parent, int sensitivity, int height, int length, boolean blockIme) {
         if (!parent.isAdded()) {
             return;
         }
@@ -56,6 +57,7 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
         bundle.putInt(KEY_BACK_SENSITIVITY, sensitivity);
         bundle.putInt(KEY_BACK_HEIGHT, height);
         bundle.putInt(KEY_HOME_HANDLE_SIZE, length);
+        bundle.putBoolean(KEY_BACK_BLOCK_IME, blockIme);
         dialog.setArguments(bundle);
         dialog.setTargetFragment(parent, 0);
         dialog.show(parent.getFragmentManager(), TAG);
@@ -74,6 +76,8 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
         seekBarSensitivity.setProgress(getArguments().getInt(KEY_BACK_SENSITIVITY));
         final SeekBar seekBarHandleSize = view.findViewById(R.id.home_handle_seekbar);
         seekBarHandleSize.setProgress(getArguments().getInt(KEY_HOME_HANDLE_SIZE));
+        final Switch blockImeSwitch = view.findViewById(R.id.back_block_ime);
+        blockImeSwitch.setChecked(getArguments().getBoolean(KEY_BACK_BLOCK_IME));
         final SeekBar seekBarHeight = view.findViewById(R.id.back_height_seekbar);
         seekBarHeight.setProgress(getArguments().getInt(KEY_BACK_HEIGHT));
         final Switch arrowSwitch = view.findViewById(R.id.back_arrow_gesture_switch);
@@ -105,6 +109,9 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                     getArguments().putInt(KEY_BACK_SENSITIVITY, sensitivity);
                     int height = seekBarHeight.getProgress();
                     getArguments().putInt(KEY_BACK_HEIGHT, height);
+                    boolean blockIme = blockImeSwitch.isChecked();
+                    getArguments().putBoolean(KEY_BACK_BLOCK_IME, blockIme);
+                    SystemNavigationGestureSettings.setBackBlockIme(getActivity(), blockIme);
                     SystemNavigationGestureSettings.setBackHeight(getActivity(), height);
                     SystemNavigationGestureSettings.setBackSensitivity(getActivity(),
                             getOverlayManager(), sensitivity);
