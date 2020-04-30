bf97e893a3aefbf556a837986eb5fe7aae57c8d2
Nicolas Dhouailly <ndhouailly@gmail.com>
2020-04-30 12:48:05 +0200

    Settings: Sync battery icon with status bar [2/2]
    Change-Id: I151aa7bb7d1bec5c6c1d6fa841e67d0d1df9bd6b

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -145,6 +145,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     BatteryTipPreferenceController mBatteryTipPreferenceController;
     private int mStatsType = BatteryStats.STATS_SINCE_CHARGED;
     private Preference mSmartCharging;
+    BatteryMeterView mBatteryView;
 
     @VisibleForTesting
     final ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
@@ -195,8 +196,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             };
 
     protected void updateViews(List<BatteryInfo> batteryInfos) {
-        final BatteryMeterView batteryView = mBatteryLayoutPref
-                .findViewById(R.id.battery_header_icon);
         final TextView percentRemaining =
                 mBatteryLayoutPref.findViewById(R.id.battery_percent);
         final TextView summary1 = mBatteryLayoutPref.findViewById(R.id.summary1);
@@ -215,8 +214,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                         PowerUtil.convertUsToMs(newInfo.remainingTimeUs)));
         summary1.setText(OldEstimateString + "\n" + NewEstimateString);
 
-        batteryView.setBatteryLevel(oldInfo.batteryLevel);
-        batteryView.setCharging(!oldInfo.discharging);
+        mBatteryView.setBatteryLevel(oldInfo.batteryLevel);
+        mBatteryView.setCharging(!oldInfo.discharging);
     }
 
     private LoaderManager.LoaderCallbacks<List<BatteryTip>> mBatteryTipsCallbacks =
@@ -259,6 +258,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
         initFeatureProvider();
         mBatteryLayoutPref = (LayoutPreference) findPreference(KEY_BATTERY_HEADER);
+        mBatteryView = mBatteryLayoutPref.findViewById(R.id.battery_header_icon);
+        mBatteryView.setDrawableStyle();
 
         mBatteryLevel = getContext().getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel) + 1;
@@ -328,6 +329,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 Global.getUriFor(Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME),
                 false,
                 mSettingsObserver);
+        mBatteryView.setDrawableStyle();
     }
 
     @Override
