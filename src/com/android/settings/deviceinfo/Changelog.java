d136dac2548630e0af3f4e02a2f1ab8adec40dca
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-09-05 23:59:12 +0530

    Move Changelog fragment to deviceinfo

--- src/com/android/settings/deviceinfo/Changelog.java
+++ src/com/android/settings/deviceinfo/Changelog.java
@@ -14,7 +14,7 @@
     limitations under the License.
  */
 
-package com.android.settings.changelog;
+package com.android.settings.deviceinfo;
 
 import android.app.Fragment;
 import android.content.res.Resources;
