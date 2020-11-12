8cd22b95ab68e15037f7aa2d96b3b13bdf802654
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-11-12 13:03:23 +0530

    Settings: Add an easter egg
    Will be expanded more soon
    
    Based on dev options countdown

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionSettings.java
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionSettings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2020 ShapeShiftOS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +17,19 @@
 
 package com.android.settings.deviceinfo.firmwareversion;
 
-import android.app.settings.SettingsEnums;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.settings.SettingsEnums;
+import android.provider.Settings;
+import android.util.Log;
 import android.provider.SearchIndexableResource;
+import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
@@ -26,15 +37,70 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 
+import androidx.preference.ListPreference;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceClickListener;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @SearchIndexable
-public class FirmwareVersionSettings extends DashboardFragment {
+public class FirmwareVersionSettings extends DashboardFragment implements OnPreferenceClickListener {
+
+    private PreferenceScreen mLogo;
+    private Toast mToast;
+    private int mLogoCountdown = 10;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        final Resources res = getResources();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mLogo = (PreferenceScreen) findPreference("ssos_logo");
+        mLogo.setOnPreferenceClickListener(this);
+        mToast = null;
+    }
 
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.firmware_version;
+    }
+
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (mLogoCountdown > 0) {
+            mLogoCountdown--;
+            if (mLogoCountdown == 0) {
+                if (mToast != null) {
+                    mToast.cancel();
+                }
+                mToast = Toast.makeText(getContext(),
+                        getResources().getString(R.string.emojii),
+                        Toast.LENGTH_SHORT);
+                mToast.show();
+            } else {
+                if (mToast != null) {
+                    mToast.cancel();
+                }
+                mToast = Toast.makeText(getContext(),
+                        getResources().getQuantityString(
+                        R.plurals.show_logo_countdown, mLogoCountdown,
+                        mLogoCountdown), Toast.LENGTH_SHORT);
+                mToast.show();
+            }
+        }
+        return true;
     }
 
     @Override
