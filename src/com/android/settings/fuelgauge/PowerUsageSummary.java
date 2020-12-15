dafbe378e4c6741f3b8bc91d6c9893ce0595e3a6
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-12-15 23:08:47 +0530

    Revert "Settings: Fix crash when going back from battery"
    This reverts commit 50d334b0b89788a49aaa9410a58a39443c9ad586.

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -409,21 +409,18 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
     @VisibleForTesting
     void initHeaderPreference() {
-        if (getContext() != null) {
-            final BatteryMeterView batteryView = (BatteryMeterView) mBatteryLayoutPref
-                  .findViewById(R.id.battery_header_icon);
-            final TextView timeText = (TextView) mBatteryLayoutPref.findViewById(R.id.battery_percent);
+        final BatteryMeterView batteryView = (BatteryMeterView) mBatteryLayoutPref
+                .findViewById(R.id.battery_header_icon);
+        final TextView timeText = (TextView) mBatteryLayoutPref.findViewById(R.id.battery_percent);
 
-            batteryView.setBatteryLevel(mBatteryLevel);
-            batteryView.setPowerSave(mPowerManager.isPowerSaveMode());
-            timeText.setText(formatBatteryPercentageText(mBatteryLevel));
-        }
+        batteryView.setBatteryLevel(mBatteryLevel);
+        batteryView.setPowerSave(mPowerManager.isPowerSaveMode());
+        timeText.setText(formatBatteryPercentageText(mBatteryLevel));
     }
 
     @VisibleForTesting
     void startBatteryHeaderAnimationIfNecessary(BatteryMeterView batteryView, TextView timeTextView,
-                int prevLevel, int currentLevel) {
-        if (getContext() != null) {
+            int prevLevel, int currentLevel) {
         mBatteryLevel = currentLevel;
         final int diff = Math.abs(prevLevel - currentLevel);
         if (diff != 0) {
@@ -442,7 +439,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             });
             animator.start();
         }
-      }
     }
 
     @VisibleForTesting
@@ -495,13 +491,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     }
 
     private CharSequence formatBatteryPercentageText(int batteryLevel) {
-        try {
-            return TextUtils.expandTemplate(getContext().getText(R.string.battery_header_title_alternate),
-                  NumberFormat.getIntegerInstance().format(batteryLevel));
-        }
-        catch (Exception e) {
-            return null;
-        }
+        return TextUtils.expandTemplate(getContext().getText(R.string.battery_header_title_alternate),
+                NumberFormat.getIntegerInstance().format(batteryLevel));
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
