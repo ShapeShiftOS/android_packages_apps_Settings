1d9241cb164e25e1feb3c75c6b16ff1a603f247f
AshutoshSundresh <ashutoshsundresh@gmail.com>
2022-06-11 11:31:59 +0530

    Revert "Use BUILD.VERSION.INCREMENTAL for search index check"
    This reverts commit 1c6d3d1be625cc495a5d911a08cf2ba4e76651c7.

--- src/com/android/settings/slices/SlicesDatabaseHelper.java
+++ src/com/android/settings/slices/SlicesDatabaseHelper.java
@@ -239,6 +239,6 @@ public class SlicesDatabaseHelper extends SQLiteOpenHelper {
 
     @VisibleForTesting
     String getBuildTag() {
-        return Build.VERSION.INCREMENTAL;
+        return Build.FINGERPRINT;
     }
 }
